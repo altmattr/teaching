fd02e725ada2420abb73a1b611f936876ccaa661
Matt Roberts <matthew.roberts@mq.edu.au>
2025-03-25 13:04:45 +1100

    getting basics from 2025

---
+++ COMP2010 - Algorithms and Data Structures/.devcontainer/install.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get install git
+apt-get install -y texlive-full
+apt-get install -y openjdk-21-jdk
---
+++ Software Tech/.devcontainer/install.sh
@@ -0,0 +1,3 @@
+apt-get update
+apt-get install -y texlive-full
+apt-get install -y openjdk-21-jdk
